18ba6452894629a0b471b9202c719a2eca55a8c3
Ша Гри <Grissshhha@outlook.com>
2025-11-18 23:25:07 +0500

    Added Couplig, core Separate

--- Assets/Scripts/RoadTrain/Wagons/Wagon.cs
+++ Assets/Scripts/RoadTrain/Wagons/Wagon.cs
@@ -1,11 +1,13 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace RoadTrane
 {
     public class Wagon : TranePart
     {
+        private const float SeparateSpeed = -0.006f;
+
         public enum Type
         {
             Regular,
@@ -19,12 +21,16 @@ namespace RoadTrane
         [SerializeField] private string Name;
         [SerializeField] private Type TypeWagon;
 
+        private Vector3 SeparateFinalPosition = new Vector3(0, -30f, 0);
+        private Coroutine _separating = null;
 
         public Transform BackCouplingPosition => _backCouplingPosition;
+
         public Transform FrontCouplingPosition => _frontÑouplingPosition;
 
-        public int IdWagon { get; private set; }
+        public bool IsSepareted { get; private set; } = false;
 
+        public int IdWagon { get; private set; }
 
         public Transform GetPointTower(int key)
         {
@@ -38,6 +44,33 @@ namespace RoadTrane
 
         public override void OnEnabled()
         {
+        }
+
+        private void OnDisable()
+        {
+            _separating = null;
+        }
+
+        public void Separate()
+        {
+            if (_separating == null && IsSepareted == false)
+            {
+                IsSepareted = true;
+                StopVibration();
+                _separating = StartCoroutine(Separating());
+            }
+        }
+
+        private IEnumerator Separating()
+        {
+            while (transform.position.y >= SeparateFinalPosition.y)
+            {
+                transform.Translate(0, SeparateSpeed, 0);
+                yield return null;
+            }
+
+            _separating = null;
+            gameObject.SetActive(false);
         }
     }
 }
